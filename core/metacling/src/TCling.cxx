13640e54a50a9a5ec3cdee717612c8eaddb341fb
Arpitha <arpitharaghunandan@gmail.com>
2019-07-12 22:31:08 +0530

    Preload Graf for the same reason as Hist and Gpad
    It does not contain identifier TFrame

--- core/metacling/src/TCling.cxx
+++ core/metacling/src/TCling.cxx
@@ -1125,7 +1125,7 @@ static void RegisterCommonCxxModules(cling::Interpreter &clingInterp)
       LoadModules(CommonModules, clingInterp);
 
       // These modules should not be preloaded but they fix issues.
-      std::vector<std::string> FIXMEModules = {"Hist", "Gpad"};
+      std::vector<std::string> FIXMEModules = {"Hist", "Gpad", "Graf"};
       LoadModules(FIXMEModules, clingInterp);
    }
 
