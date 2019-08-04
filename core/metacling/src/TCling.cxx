c91b017cb640fb39bca0099c334e15d42718cfce
Vassil Vassilev <v.g.vassilev@gmail.com>
2019-08-05 00:41:56 +0300

    Revert "Adding Geom to FIXMEModules"
    This reverts commit 078f868a9971a6eb815ac0db2fa7cacb895c8d25.

--- core/metacling/src/TCling.cxx
+++ core/metacling/src/TCling.cxx
@@ -1170,7 +1170,7 @@ static void RegisterCommonCxxModules(cling::Interpreter &clingInterp)
       LoadModules(CommonModules, clingInterp);
 
       // These modules should not be preloaded but they fix issues.
-      std::vector<std::string> FIXMEModules = {"Hist", "Gpad", "Graf", "GenVector", "ROOTDataFrame", "Geom"};
+      std::vector<std::string> FIXMEModules = {"Hist", "Gpad", "Graf", "GenVector", "ROOTDataFrame"};
       LoadModules(FIXMEModules, clingInterp);
 
       loadGlobalModuleIndex(SourceLocation(), clingInterp);
